dbe4c2fa8c4b2f6b09950adb80e0c52ff3961c94
Kanashimi <45765913+nazartnk@users.noreply.github.com>
2021-04-05 10:28:01 +0300

    Add files via upload

---
+++ 4 семестр/Питон/main.py
@@ -0,0 +1,31 @@
+# 1
+s = input('Введите строку:')
+print(s[2])
+print(s[-2])
+print(s[:5])
+print(s[:-2])
+print(s[::2])
+print(s[1::2])
+print(s[::-1])
+print(s[::-2])
+print(len(s))
+
+# 2
+# print(input('Введите строку:').count(' ') + 1)
+
+# 3
+# s = input('Введите 2 слова:')
+# fw = s[:s.find(' ')]
+# sw = s[s.find(' ') + 1:]
+# print(sw + ' ' + fw)
+
+# 4
+# s = input('Введите слова с буквой h:')
+# s = s[:s.find('h')] + s[s.rfind('h') + 1:]
+# print(s)
+
+# 5
+# print(input('Введите слово с цифрой 1:').replace('1', 'один'))
+
+# 6
+# print(input('Введите слова с символом @;').replace('@', ''))
